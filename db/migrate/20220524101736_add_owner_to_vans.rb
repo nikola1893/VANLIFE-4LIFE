3593aa322ebac2f981e438cfad71c39c36f67486
Mael Micout <micoutmael3@gmail.com>
2022-05-24 12:27:22 +0200

    seedsupdated

---
+++ db/migrate/20220524101736_add_owner_to_vans.rb
@@ -0,0 +1,5 @@
+class AddOwnerToVans < ActiveRecord::Migration[6.1]
+  def change
+    add_column :vans, :title, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Van.destroy_all
+User.destroy_all
 
- User.create!( first_name: 'Mael', last_name: 'Micout', email: 'mael124@gmail.com', password: 'Lewagon906', owner: true)
+ mael = User.create!( first_name: 'Mael', last_name: 'Micout', email: 'mael124@gmail.com', password: 'Lewagon906', owner: true)
  User.create!( first_name: 'Denis', last_name: 'deav', email: 'denis124@gmail.com', password: 'Lewagon906', owner: false)
 
- Van.create!( user_id: 1, description:'', available: true, price_per_day: 100)
- Van.create!( user_id: 1, description:'', available: true, price_per_day: 200)
- Van.create!( user_id: 1, description:'', available: true, price_per_day: 150)
- Van.create!( user_id: 1, description:'', available: true, price_per_day: 110)
- Van.create!( user_id: 1, description:'', available: true, price_per_day: 120)
- Van.create!( user_id: 1, description:'', available: true, price_per_day: 130)
- Van.create!( user_id: 1, description:'', available: true, price_per_day: 140)
- Van.create!( user_id: 1, description:'', available: true, price_per_day: 135)
- Van.create!( user_id: 1, description:'', available: true, price_per_day: 125)
- Van.create!( user_id: 1, description:'', available: true, price_per_day: 115)
+ Van.create!( user_id: mael.id, title:'Vans 1', description:'', available: true, price_per_day: 100)
+ Van.create!( user_id: mael.id, title:'Van 2', description:'', available: true, price_per_day: 200)
+ Van.create!( user_id: mael.id, title:'Van 3', description:'', available: true, price_per_day: 150)
+ Van.create!( user_id: mael.id, title:'Van 4', description:'', available: true, price_per_day: 110)
+ Van.create!( user_id: mael.id, title:'Van 5', description:'', available: true, price_per_day: 120)
+ Van.create!( user_id: mael.id, title:'Van 6', description:'', available: true, price_per_day: 130)
+ Van.create!( user_id: mael.id, title:'Van 7', description:'', available: true, price_per_day: 140)
+ Van.create!( user_id: mael.id, title:'Van 8', description:'', available: true, price_per_day: 135)
+ Van.create!( user_id: mael.id, title:'Van 9', description:'', available: true, price_per_day: 125)
+ Van.create!( user_id: mael.id, title:'Van 10', description:'', available: true, price_per_day: 115)
