96129b101c989abd49a135b24eb07c267c3b9e95
Mael Micout <micoutmael3@gmail.com>
2022-05-24 10:55:25 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ User.create( first_name: 'Mael', last_name: 'Micout', email: 'micoutmael3@gmail.com', password: 'Lewagon906', owner: true)
+ User.create( first_name: 'Denis', last_name: 'deav', email: 'denis@gmail.com', password: 'Lewagon906', owner: false)
+
+ Van.create( user_id: 1, descritpion:'', available: true, price_per_day: 100)
