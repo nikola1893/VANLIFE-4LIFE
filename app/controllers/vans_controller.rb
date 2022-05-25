03df96754b0439a2b287941cc197ce13a835afb0
Nikola Mitev <mitev.nkl@gmail.com>
2022-05-25 12:54:52 +0200

    new view

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -9,5 +9,20 @@ class VansController < ApplicationController
 
   def new
     @van = Van.new
+    if @van.save
+      redirect_to @van, notice: 'Van was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def create
+    @van = Van.new()
+  end
+
+  private
+
+  def van_params
+    params.require(:van).permit(:title, :descriptiom, :price_per_day)
   end
 end
