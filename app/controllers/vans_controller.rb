d61f4985865da1bab1c9c2727bd451b4f8b2c6d0
Mael Micout <micoutmael3@gmail.com>
2022-05-26 12:11:53 +0200

    map done

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -5,5 +5,12 @@ class VansController < ApplicationController
 
   def show
     @van = Van.find(params[:id])
+
+    @markers = Van.all.geocoded.map do |van|
+      {
+        lat: van.latitude,
+        lng: van.longitude
+      }
+    end
   end
 end
